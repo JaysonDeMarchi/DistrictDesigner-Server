12b2c2b236dbdd9fabc2d2176174a510860ce326
Jayson DeMarchi <jayson.demarchi@gmail.com>
2018-11-26 11:29:45 -0500

    Add required request Enums
    A startAlgorithm request sent to server will require
    a state short name, a set of metrics with associated
    weights, and an algorithm type. This commit will add
    the associated enums for:
    - ShortName
    - Metric
    - AlgorithmType

---
+++ src/main/java/Enums/Metric.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Enums;
+
+/**
+ *
+ * @author Jayson
+ */
+public enum Metric {
+  COMPACTNESS,
+  PARTISAN_GERRYMANDERING,
+  POPULATION_EQUALITY,
+  WASTED_VOTERS,
+}
---
+++ src/main/java/Enums/ShortName.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Enums;
+
+/**
+ *
+ * @author Jayson
+ */
+public enum ShortName {
+  AK,
+  AL,
+  AR,
+  AZ,
+  CA,
+  CO,
+  CT,
+  DE,
+  FL,
+  GA,
+  HI,
+  IA,
+  ID,
+  IL,
+  IN,
+  KS,
+  KY,
+  LA,
+  MA,
+  MD,
+  ME,
+  MI,
+  MN,
+  MO,
+  MS,
+  MT,
+  NC,
+  ND,
+  NE,
+  NH,
+  NJ,
+  NM,
+  NV,
+  NY,
+  OH,
+  OK,
+  OR,
+  PA,
+  RI,
+  SC,
+  SD,
+  TN,
+  TX,
+  UT,
+  VA,
+  VT,
+  WA,
+  WI,
+  WV,
+  WY,
+}
