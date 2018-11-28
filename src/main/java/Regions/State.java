1341c20914894fa2e47528300d184cf3f106842d
Jayson DeMarchi <jayson.demarchi@gmail.com>
2018-11-28 03:16:44 -0500

    Create State Object
    Creating the state object to be used during the
    run of the associated algorithm.
    Currently Contains:
     - ShortName
       - E.g. "NY" for the state New York

---
+++ src/main/java/Regions/State.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Regions;
+
+import Enums.ShortName;
+
+/**
+ *
+ * @author Jayson
+ */
+public class State {
+
+  ShortName shortName;
+
+  public State(ShortName shortName) {
+    this.shortName = shortName;
+  }
+}
