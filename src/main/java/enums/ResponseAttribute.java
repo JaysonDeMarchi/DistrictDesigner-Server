e1768db07ea02a46cb9f2459ad1ff5f0977e6b06
Jayson DeMarchi <jayson.demarchi@gmail.com>
2018-12-02 20:13:32 -0500

    Add Enums for Update Responses
    Adding all parameters expected to be sent to the
    client when a GET request is made for updates
    to the algorithm.

--- src/main/java/enums/ResponseAttribute.java
+++ src/main/java/enums/ResponseAttribute.java
@@ -5,5 +5,10 @@ package enums;
  * @author Jayson
  */
 public enum ResponseAttribute {
-  ALGO_STARTED;
+  ALGO_STARTED,
+  UPDATED_PRECINCTS,
+  PRECINCT_ID,
+  OLD_DISTRICT_ID,
+  NEW_DISTRICT_ID,
+  SUCCESS_STATUS;
 }
