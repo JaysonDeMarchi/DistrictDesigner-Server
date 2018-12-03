41f4489326af621fe680ad67d9d211a3a55363b7
Jayson DeMarchi <jayson.demarchi@gmail.com>
2018-12-03 11:13:08 -0500

    Create StopAlgorithm Servlet
    Creates a servlet at connetion URL + `/StopAlgorithm`
    that invalidates the current session with the client,
    and sends a response with boolean attribute ALGO_STOPPED
    
    that evaluates to true on success.

---
+++ src/main/java/servlets/StopAlgorithm.java
@@ -0,0 +1,81 @@
+package servlets;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import enums.ResponseAttribute;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Jayson
+ */
+@WebServlet(name = "StopAlgorithm", urlPatterns = {"/StopAlgorithm"})
+public class StopAlgorithm extends HttpServlet {
+
+  ObjectMapper mapper = new ObjectMapper();
+
+  protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    HttpSession session = request.getSession();
+    session.invalidate();
+    System.out.println("Session invalidated!");
+    processResponse(request, response, true);
+  }
+
+  private void processResponse(HttpServletRequest request, HttpServletResponse response, Boolean status) throws IOException {
+    ObjectNode responseBody = mapper.createObjectNode();
+    PrintWriter pw = response.getWriter();
+
+    response.setContentType("application/json;charset=UTF-8");
+    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+    responseBody.put(ResponseAttribute.ALGO_STOPPED.toString(), status);
+    pw.print(responseBody.toString());
+  }
+
+  // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+  /**
+   * Handles the HTTP <code>GET</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  /**
+   * Handles the HTTP <code>POST</code> method.
+   *
+   * @param request servlet request
+   * @param response servlet response
+   * @throws ServletException if a servlet-specific error occurs
+   * @throws IOException if an I/O error occurs
+   */
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException {
+    processRequest(request, response);
+  }
+
+  /**
+   * Returns a short description of the servlet.
+   *
+   * @return a String containing servlet description
+   */
+  @Override
+  public String getServletInfo() {
+    return "Short description";
+  }// </editor-fold>
+
+}
