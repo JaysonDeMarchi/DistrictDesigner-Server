7f3b49d8b4c6d6b4220787ca07853ea27f5afa17
Jayson DeMarchi <jayson.demarchi@gmail.com>
2018-12-02 20:16:26 -0500

    Add UpdateManger to Algorithms
    An updateManager has been added to all Algorithm
    objects as well as a run() method. The run() method
    will eventually perform the algorithm over the state
    but for now it just returns the UpdateManager.

--- src/main/java/algorithms/Algorithm.java
+++ src/main/java/algorithms/Algorithm.java
@@ -4,6 +4,7 @@ import enums.Metric;
 import enums.ShortName;
 import regions.State;
 import java.util.Map;
+import managers.UpdateManager;
 
 /**
  *
@@ -13,11 +14,20 @@ public abstract class Algorithm {
 
   State state;
   Map<Metric, Float> weights;
+  UpdateManager updateManager;
 
   public Algorithm(ShortName shortName, Map<Metric, Float> weights) {
     this.state = new State(shortName);
+    this.updateManager = new UpdateManager();
     this.weights = weights;
   }
 
   public abstract Boolean start();
+
+  public abstract UpdateManager run();
+
+  public UpdateManager getUpdateManager() {
+    return this.updateManager;
+  }
+
 }
