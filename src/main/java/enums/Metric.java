253043d80f53a869646003275455363d7b164601
Jayson DeMarchi <jayson.demarchi@gmail.com>
2018-12-04 06:33:30 -0500

    Add method to calculate Metrics
    Create the first method associated with the metrics
    inside the Metric Enum.

--- src/main/java/enums/Metric.java
+++ src/main/java/enums/Metric.java
@@ -1,12 +1,27 @@
 package enums;
 
+import java.time.Year;
+import metrics.measures.Schwartzberg;
+import metrics.results.DistrictMetricResult;
+import regions.District;
+
 /**
  *
  * @author Jayson
  */
 public enum Metric {
-  COMPACTNESS,
   PARTISAN_GERRYMANDERING,
   POPULATION_EQUALITY,
   WASTED_VOTERS,
+  POLSBY_POPPER_COMPACTNESS,
+  SCHWARTZBERG_COMPACTNESS {
+    @Override
+    public DistrictMetricResult calculate(District district, Year year) {
+      return Schwartzberg.calculate(district, year);
+    }
+  };
+
+  public DistrictMetricResult calculate(District district, Year year) {
+    return new DistrictMetricResult();
+  }
 }
