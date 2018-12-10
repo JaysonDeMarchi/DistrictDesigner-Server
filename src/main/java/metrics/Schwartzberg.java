5b6ddf67fd936f6af90c85f8c71e0f7a39eb228c
HQ Zhu <hqzhu@HQs-MacBook-Pro.nyc.rr.com>
2018-12-10 18:23:58 -0500

    cleaned master

---
+++ src/main/java/metrics/Schwartzberg.java
@@ -0,0 +1,19 @@
+package metrics;
+
+/**
+ *
+ * @author Jayson
+ */
+public class Schwartzberg {
+
+  public Double calculate(double area, double perimeter) {
+    // equalAreaRadius = r = sqrt(A/PI)
+    double r = Math.sqrt(area / Math.PI);
+    // equalAreaPerimeter = C = 2pi * r (Circumference)
+    double equalAreaPerimeter = 2 * Math.PI * r;
+    // Schwartzberg score = 1 / (Perimeter of district / C )
+    double score = 1 / (perimeter / equalAreaPerimeter);
+    return score;
+  }
+
+}
