e729947974128462735a45470639ff309e81489a
Jayson DeMarchi <jayson.demarchi@gmail.com>
2018-12-10 13:52:30 -0500

    Handle grouped Precincts
    Some precincts were stored as ward 1-3
    while in the geojson file they should be
    separate. This commit will break up those
    precincts.

--- src/main/python/standardizeKeys.py
+++ src/main/python/standardizeKeys.py
@@ -35,17 +35,23 @@ def precinctNameIsParseable(precinct_name):
 def convertPrecinct(precinct, headers):
   name_index = headers.index('precinct')
   converted_precincts = []
-  converted_name = convertPrecinctName(precinct[name_index]).split()
-  ward_num = converted_name[-1]
+  converted_name = convertPrecinctName(precinct[name_index])
+  name_attributes = converted_name.split()
+  ward_num = name_attributes[-1]
 
-  # if ('-' in ward_num):
-  #   start = int(''.join(ward_num.split('-')))
-  #   end = int(''.join(ward_num.split('-')))
-  #   for i in range(start,end):
-  #     precinct[name_index] = converted_name[:-1] + i
-  # else:
-  precinct[name_index] = converted_name
-  converted_precincts.append(precinct)
+  if ('-' in ward_num):
+    try:
+      start = int(ward_num.split('-')[0])
+      end = int(ward_num.split('-')[-1])
+      for i in range(start,end + 1):
+        generated_precinct = precinct[:]
+        generated_precinct[name_index] = reduce(lambda x,y: x + ' ' + y, name_attributes[:-1]) + ' ' + str(i)
+        converted_precincts.append(generated_precinct)
+    except ValueError:
+      pass
+  else:
+    precinct[name_index] = converted_name
+    converted_precincts.append(precinct)
   return converted_precincts
 
 def convertPrecinctName(precinct_name):
@@ -62,9 +68,11 @@ if __name__ == '__main__':
     print(headers)
     name_index = headers.index('precinct')
 
-    with open('resources/2016/standardized/senate/' + 'WI' +  '.csv', 'w') as out:
+    with open('resources/2016/standardized/local/' + 'WI' +  '.csv', 'w') as out:
       csv_out = csv.writer(out)
-      csv_out.writerow(['precinct'])
+      csv_out.writerow(headers)
       for precinct in precincts:
-        map(lambda row: csv_out.writerow([row[name_index]]), convertPrecinct(precinct,headers))
+        expanded_precinct = convertPrecinct(precinct,headers)
+        for converted_precinct in expanded_precinct:
+          csv_out.writerow(converted_precinct)
       out.close()
