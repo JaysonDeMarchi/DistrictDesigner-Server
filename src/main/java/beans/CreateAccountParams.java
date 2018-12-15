b809e6972ec4a0206c5fe535b2379edac928b8ed
Jayson DeMarchi <jayson.demarchi@gmail.com>
2018-12-15 16:13:27 -0500

    Add getters/setters to CreateAccount Bean

--- src/main/java/beans/CreateAccountParams.java
+++ src/main/java/beans/CreateAccountParams.java
@@ -18,4 +18,20 @@ public class CreateAccountParams {
     this.password = password;
   }
 
+  public String getUsername() {
+    return this.username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return this.password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
 }
