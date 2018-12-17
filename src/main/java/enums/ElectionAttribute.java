77ca705145fb3610d0a95fd6f0788e6eac9574eb
Jayson DeMarchi <jayson.demarchi@gmail.com>
2018-12-17 05:52:53 -0500

    Add values to ElectionAttributes

--- src/main/java/enums/ElectionAttribute.java
+++ src/main/java/enums/ElectionAttribute.java
@@ -1,12 +1,30 @@
 package enums;
 
+import electionResults.Election;
+
 /**
  *
  * @author Jayson
  */
 public enum ElectionAttribute {
-  OFFICE,
-  CANDIDATE,
-  PARTY,
-  VOTER_COUNT;
+  CANDIDATE {
+    @Override
+    public String getValue(Election election) {
+      return election.getCandidate();
+    }
+  },
+  PARTY {
+    @Override
+    public String getValue(Election election) {
+      return election.getParty();
+    }
+  },
+  VOTER_COUNT {
+    @Override
+    public String getValue(Election election) {
+      return election.getVoterCount();
+    }
+  };
+
+  public abstract String getValue(Election election);
 }
